edca6eae41eccfa56e1da8428f23fcdf0cd1d3d5
ariqah <20023243@rp.edu.sg>
2021-07-07 20:30:38 +0800

    commit demo simple save

---
+++ app/src/main/java/sg/edu/rp/c346/id20023243/demosimplesave/MainActivity.java
@@ -0,0 +1,43 @@
+package sg.edu.rp.c346.id20023243.demosimplesave;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume(); //super ALWAYS leave on first line, put code below
+
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE);
+        //obtain SharedPreferences instance
+
+        String msg = prefs.getString("greetings","No greetings");
+        //retrieve saved data from SharedPreferences; if no data display default value
+
+        Toast.makeText(MainActivity.this, msg, Toast.LENGTH_LONG).show();
+        //toast the retrieved data
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        SharedPreferences prefs = getPreferences(MODE_PRIVATE); //obtain sharedpreferences instance
+        SharedPreferences.Editor prefEdit = prefs.edit();
+        prefEdit.putString("greetings","Hello!");
+        prefEdit.commit();
+
+    }
+
+}
